e1b6d51323aaf8b352ce57ceea2904e24731cd3e
Nat York <natalie@freerunningtechnologies.com>
2017-03-03 10:02:43 -0800

    Remove instance variable points

--- crib_hand.rb
+++ crib_hand.rb
@@ -9,36 +9,35 @@ class CribHand
     @hand = hand
     @values = @hand.map { |card| card[0] }
     @suits = @hand.map { |card| card[1] }
-    @points = 0
   end
 
   def points
-    @points = 0
+    points = 0
 
     # fifteen_twos
     faces_to_tens = @values.map { |card| [11, 12, 13].include?(card) ? 10 : card }
     hand_combinations = (2..5).flat_map { |size| faces_to_tens.combination(size).to_a }
-    @points += 2 * hand_combinations.count { |card_combo| card_combo.sum == 15 }
+    points += 2 * hand_combinations.count { |card_combo| card_combo.sum == 15 }
 
     # runs
     hand_combinations = (3..5).flat_map { |size| @values.combination(size).to_a.sort }
     run_list = hand_combinations.select { |card_combo| card_combo.each_cons(2).all? {|a, b| b == a + 1 } }
     max_run_length = run_list.length > 0 ? run_list.max_by(&:length).length : 0
-    @points +=  max_run_length * run_list.count { |run| run.length == max_run_length }
+    points +=  max_run_length * run_list.count { |run| run.length == max_run_length }
 
     # pairs
     hand_combinations = @values.combination(2).to_a
-    @points += 2 * hand_combinations.count { |card_combo| card_combo.all? { |card| card == card_combo[0] } }
+    points += 2 * hand_combinations.count { |card_combo| card_combo.all? { |card| card == card_combo[0] } }
 
     # flush
-    @points += 4 if @suits[1, 4].all? { |card| card == @suits[1] }
-    @points += 1 if @suits.all? { |card| card == @suits[0] }
+    points += 4 if @suits[1, 4].all? { |card| card == @suits[1] }
+    points += 1 if @suits.all? { |card| card == @suits[0] }
 
     # nob
     starter_suit = @suits[0]
     players_hand = @hand[1, 4]
-    @points += 1 if players_hand.include?([11, starter_suit])
+    points += 1 if players_hand.include?([11, starter_suit])
 
-    @points
+    points
   end
 end
